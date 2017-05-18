f7360a6b2df580eaab1a27a7658c04a301272f89
Yuri Artemev <i@artemeff.com>
2017-05-18 13:04:50 +0300

    add assert_match_in? macro

--- lib/ex_unit_assertions.ex
+++ lib/ex_unit_assertions.ex
@@ -1,18 +1,95 @@
 defmodule ExUnitAssertions do
-  @moduledoc """
-  Documentation for ExUnitAssertions.
-  """
-
   @doc """
-  Hello world.
+  Matches pattern in list:
 
-  ## Examples
+      > assert_match_in?(1, [1, 2, 3])
+      true
 
-      iex> ExUnitAssertions.hello
-      :world
+      > assert_match_in?(%{a: 1}, [%{a: 1, b: 2}, %{a: 2, b: 3}])
+      true
 
+      > assert_match_in?(%{a: 1}, [%{a: 2, b: 2}, %{a: 2, b: 3}])
+      false
+
+      > a = 1
+      > assert_match_in?(%{a: ^a}, [%{a: 1, b: 2}, %{a: 2, b: 3}])
+      true
+
+      > assert_match_in?(%{a: 1, b: b}, [%{a: 1, b: 2}, %{a: 2, b: 3}])
+      true
+
+      > b
+      2
   """
-  def hello do
-    :world
+  @spec assert_match_in?(term, list(term)) :: true | false
+  defmacro assert_match_in?(pattern, expr) do
+    left = Macro.expand(pattern, __CALLER__)
+    vars = collect_vars_from_pattern(left)
+    pins = collect_pins_from_pattern(left)
+
+    quote do
+      left = unquote(Macro.escape(left))
+      expr = unquote(expr)
+
+      matching =
+        Enum.reduce(expr, nil, fn
+          (element, nil) ->
+            case element do
+              unquote(pattern) ->
+                _ = unquote(vars)
+                element
+              _ ->
+                nil
+            end
+          (element, acc) ->
+            acc
+        end)
+
+      if is_nil(matching) do
+        raise ExUnit.AssertionError,
+          right: expr,
+          expr: left,
+          message: "match_in? failed" <>
+                    ExUnit.Assertions.__pins__(unquote(pins))
+      end
+
+      assert unquote(pattern) = matching
+      matching
+    end
+  end
+
+  defp collect_vars_from_pattern({:when, _, [left, right]}) do
+    pattern = collect_vars_from_pattern(left)
+    for {name, _, context} = var <- collect_vars_from_pattern(right),
+      Enum.any?(pattern, &match?({^name, _, ^context}, &1)),
+      into: pattern,
+      do: var
+  end
+
+  defp collect_vars_from_pattern(expr) do
+    Macro.prewalk(expr, [], fn
+      {:::, _, [left, _]}, acc ->
+        {[left], acc}
+      {skip, _, [_]}, acc when skip in [:^, :@] ->
+        {:ok, acc}
+      {:_, _, context}, acc when is_atom(context) ->
+        {:ok, acc}
+      {name, meta, context}, acc when is_atom(name) and is_atom(context) ->
+        {:ok, [{name, [generated: true] ++ meta, context} | acc]}
+      node, acc ->
+        {node, acc}
+    end)
+    |> elem(1)
+  end
+
+  defp collect_pins_from_pattern(expr) do
+    {_, pins} =
+      Macro.prewalk(expr, [], fn
+        {:^, _, [{name, _, _} = var]}, acc ->
+          {:ok, [{name, var} | acc]}
+        form, acc ->
+          {form, acc}
+      end)
+    Enum.uniq_by(pins, &elem(&1, 0))
   end
 end
--- test/ex_unit_assertions_test.exs
+++ test/ex_unit_assertions_test.exs
@@ -1,8 +1,44 @@
 defmodule ExUnitAssertionsTest do
   use ExUnit.Case
-  doctest ExUnitAssertions
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import ExUnitAssertions
+
+  describe "#assert_match_in?" do
+    test "simple list" do
+      assert 1 == assert_match_in? 1, [1, 2, 3]
+    end
+
+    test "complex pattern" do
+      assert %{a: 1, b: 2}
+          == assert_match_in? %{a: 1}, [%{a: 0, b: 0}, %{a: 1, b: 2}, %{a: 2, b: 3}]
+    end
+
+    test "fails" do
+      assert_raise ExUnit.AssertionError, fn ->
+        assert_match_in? 1, [2, 3, 4]
+      end
+
+      assert_raise ExUnit.AssertionError, fn ->
+        assert_match_in? %{a: 1}, [%{a: 2, b: 2}, %{a: 2, b: 3}]
+      end
+    end
+
+    test "with pinned variables" do
+      a = 1
+
+      assert 1
+          == assert_match_in? ^a, [1, 2, 3]
+
+      assert %{a: 1, b: 2}
+          == assert_match_in? %{a: ^a}, [%{a: 1, b: 2}, %{a: 2, b: 3}]
+    end
+
+    test "with bounds" do
+      assert_match_in? %{a: 1, b: b1}, [%{a: 1, b: 2}, %{a: 2, b: 3}]
+      assert_match_in? %{a: 2, b: b2}, [%{a: 1, b: 2}, %{a: 2, b: 3}]
+
+      assert b1 == 2
+      assert b2 == 3
+    end
   end
 end
