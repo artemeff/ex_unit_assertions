0ae50525d8cd8a5898dcd834cb0ee1a237c9126d
Yuri Artemev <i@artemeff.com>
2017-05-18 13:16:28 +0300

    publish package

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule ExUnitAssertions.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -15,6 +17,16 @@ defmodule ExUnitAssertions.Mixfile do
   end
 
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+
+  defp description do
+    "useful ExUnit assertions"
+  end
+
+  defp package do
+    [maintainers: ["Yuri Artemev"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/artemeff/ex_unit_assertions"}]
   end
 end
